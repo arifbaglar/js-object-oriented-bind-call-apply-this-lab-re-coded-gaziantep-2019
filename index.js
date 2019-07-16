db1f8dcd79ae9a3042b297a1963c89e4c5261194
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-07-16 02:59:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function setThisWithApply(fn, thisValue, arg){
   return fn.apply(thisValue,arg);
 }
 function returnNewFunctionOf(functionToBeCopied, thisValue){
-  this.functionToBeCopied= newfunction.apply(thisValue);
-  return newfunction;
+  this.functionToBeCopied= newfunction;
+  return newfunction.apply(thisValue);
 }
